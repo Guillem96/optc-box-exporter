3d205e81ac3955b799e32133d795c37fd3715ea3
Guillem96 <guillem.orellana@gmail.com>
2020-10-18 11:23:22 +0200

    First version of application

--- optcbx/matcher.py
+++ optcbx/matcher.py
@@ -11,18 +11,27 @@ from optcbx import detect_characters
 from optcbx.units import parse_units, Character
 
 
+# Keep computational expensive variables in memory
 _portraits_paths = None
-_portraits = None
+_portraits = {}
 _units = None
 _units_ids = None
+
 
 FindCharactersResult = Union[List[int], Tuple[List[int], np.ndarray]]
 
 
-def find_characters_from_screenshot(screenshot: np.ndarray) -> List[Character]:
+def find_characters_from_screenshot(
+        screenshot: np.ndarray,
+        image_size: Union[int, Tuple[int, int]] = 64,
+        return_thumbnails: bool = False) -> List[Character]:
+
+    if isinstance(image_size, int):
+        image_size = (image_size,) * 2
+
     global _units, _units_ids
 
-    characters = detect_characters(screenshot)
+    characters = detect_characters(screenshot, image_size)
     id_matches = find_characters_ids(characters)
 
     if _units is None:
@@ -31,30 +40,36 @@ def find_characters_from_screenshot(screenshot: np.ndarray) -> List[Character]:
         _units_ids = np.array([o.number for o in _units])
 
     id_matches = [(_units_ids == i).argmax() for i in id_matches]
-    return [_units[i] for i in id_matches]
+    matched_units = [_units[i] for i in id_matches]
+
+    if not return_thumbnails:
+        return matched_units
+    else:
+        return matched_units, characters
 
 
 def find_characters_ids(
-        characters: np.ndarray, 
+        characters: np.ndarray,
         return_portraits: bool = False) -> FindCharactersResult:
+
+    image_size = characters.shape[1:3]
 
     global _portraits, _portraits_paths
 
     if _portraits_paths is None:
         _portraits_paths = list(Path('data/Portrait').glob('*.png'))
 
-    if _portraits is None:
-        _portraits = np.array([_load_im(o, characters.shape[1:3]) 
-                               for o in tqdm.tqdm(_portraits_paths)])
+    if _portraits.get(image_size) is None:
+        _portraits[image_size] = np.array(
+            [_load_im(o, image_size) for o in tqdm.tqdm(_portraits_paths)])
 
-    best_matches = _top_similarities(characters, _portraits)
+    best_matches = _top_similarities(characters, _portraits[image_size])
     ids = [int(_portraits_paths[i].stem) for i in best_matches]
-    
 
     if not return_portraits:
         return ids
     else:
-        return ids, _portraits[best_matches]
+        return ids, _portraits[image_size][best_matches]
 
 
 def _load_im(path, size):
